618bb7981453b99be221e7135290cd2deacf3f01
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-10-20 09:48:54 -0500

    feat: created login endpoints

--- src/main/java/com/example/client_register/service/ClienteService.java
+++ src/main/java/com/example/client_register/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.example.client_register.service;
 
+import com.example.client_register.dto.LoginResponse;
 import com.example.client_register.event.ClienteCreadoEvent;
 import com.example.client_register.model.Cliente;
 import com.example.client_register.repository.ClienteRepository;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -39,5 +41,20 @@ public class ClienteService {
         );
         rabbitTemplate.convertAndSend("cliente.exchange", "cliente.creado", event);
         return savedCliente;
+    }
+
+    public LoginResponse login(String correo, String contrasenia) {
+        Optional<Cliente> clienteOptional = clienteRepository.findByEmail(correo);
+
+        if (clienteOptional.isPresent()) {
+            Cliente cliente = clienteOptional.get();
+            if (cliente.getContrasenia().equals(contrasenia)) {
+                return new LoginResponse(true, "Inicio de sesión exitoso", cliente);
+            } else {
+                return new LoginResponse(false, "Contraseña incorrecta", null);
+            }
+        } else {
+            return new LoginResponse(false, "Correo no registrado", null);
+        }
     }
 }
